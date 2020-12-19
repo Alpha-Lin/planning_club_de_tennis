3e27280ae8db076adcaa9bc51ea22c04e2a6a447
Alpha_Lin <alpha_lin@protonmail.com>
2020-12-19 13:51:30 +0100

    Commit initiale

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <label for="nom">Nom : </label>
+        <input name="nom" id="nom" type="text">
+        <label for="prénom">Prénom : </label>
+        <input name="prénom" id="prénom" type="text">
+        <label for="password">Mot de passe : </label> <!-- faire une slice -->
+        <input name="password" id="password" type="password">
+
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
